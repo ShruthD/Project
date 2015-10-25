5dbcb80f0312c4becc5fb258ba2083590d549b96
ShruthD <dogra170497@gmail.com>
2015-10-25 12:36:11 +0530

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -454,5 +454,48 @@ int solvePuzzle ( PUZZLE *puzzle, HISTORY *hist ) {
 }
 
 int main() {
+	PUZZLE	*puzzle;
+	int	technique = 0;
+	int	difficulty = 0;
+	int	row, status = 1;
+	int	i = 0;
+	char clues[] = {5,3,0,0,7,0,0,0,0,
+		   6,0,0,1,9,5,0,0,0,
+		   0,9,8,0,0,0,0,6,0,
+		   8,0,0,0,6,0,0,0,3,
+		   4,0,0,8,0,3,0,0,1,
+		   7,0,0,0,2,0,0,0,6,
+		   0,6,0,0,0,0,2,8,0,
+		   0,0,0,4,1,9,0,0,5,
+		   0,0,0,0,8,0,0,7,9,0}; 
+	
+	if ( puzzle = openPuzzleSolver(clues)) {
+		HISTORY	*hist = NULL;
+		hist = openHistory ();
+		status = solvePuzzle ( puzzle, hist );
+		if ( status == 1 ) {
+			difficulty = puzzle->difficulty;
+			technique = puzzle->technique;
+		}
+		if ( hist ) {
+			CELL	*coords;
+			int	go = 1;
+			if ( status > 1 )
+			while ( go && ( coords = popHistory ( hist ) ) ) {
+				int	row = coords->row;
+				int	column = coords->column;
+				int	entry = puzzle->grid [row][column];
+			}
+		}
+	}
+	for ( i = 0; row < 9; ++row ) {
+			int	column;
+			printf("{ ");
+			for ( column = 0; column < 9; ++column ) {
+				printf("%d ", clues[i++]);
+			}
+			printf("}\n");
+	}
+	closePuzzle(puzzle);
 	return 0;
 }
