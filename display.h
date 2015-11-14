749e814eb24bf707f3506e9b9337c94e84957536
ShruthD <dogra170497@gmail.com>
2015-11-14 09:04:01 +0530

    Update display.h

--- display.h
+++ display.h
@@ -1,14 +1,11 @@
 #include	<curses.h>
-
 #define		PUZZLE_TITLE	"Sudoku Solver"
-
 #define		ESC		27	
 #define		HT		0x09	
 #define		CR		0x0D	
 #define		DEL		0x7F	
 #define		BIG		0x1
 #define		isBig(a)	((a)->flags&BIG)
-
 typedef struct {
 	int	y0;	
 	int	x0;	
@@ -19,9 +16,7 @@ typedef struct {
 	short	blockMasks [3][3];
 	char	board [9][9];
 } DISPLAY;
-
 DISPLAY	*openDisplay ( int );
-
 extern const char	*gameName;
 extern const char	*instructions1;
 extern const char	*instructions2;
@@ -33,7 +28,6 @@ extern const char	*noSolution;
 extern const char	*solved;
 extern const char	*toExit;
 extern const char	*solveAnyway;
-
 int	confirm ( DISPLAY *, const char * );
 void	displayCaptions ( DISPLAY *, const char *, const char * );
 int	displayEntry ( DISPLAY *, int, int, int );
